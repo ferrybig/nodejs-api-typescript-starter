e5435a0f0af27e2bdc9df137a242b17b78de6990
Christopher Escalon <escalonchristopher@gmail.com>
2019-10-30 15:11:46 -0600

    Fixing tslint warnings

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,32 @@
-import "reflect-metadata";
+import bodyParser from "body-parser";
+import compression from "compression";
+import cors from "cors";
 import dotenv from "dotenv";
 import express, { Application } from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+import helmet from "helmet";
 import morgan from "morgan";
-// import helmet from "helmet";
-// import compression from "compression";
+import "reflect-metadata";
 import {
-  useExpressServer,
-  useContainer as routingUseContainer
+  useContainer as routingUseContainer,
+  useExpressServer
 } from "routing-controllers";
+import signale from "signale";
 import swaggerUi from "swagger-ui-express";
-import swaggerDocument from "./swagger.json";
-
-import { useContainer as ormUseContainer } from "typeorm";
 import { Container } from "typedi";
-
-import { createConnection } from "typeorm";
+import { createConnection, useContainer as ormUseContainer } from "typeorm";
+import swaggerDocument from "./swagger.json";
 
 dotenv.config();
 
 export default class Server {
+  public static bootstrap(): Application {
+    this.configureServices();
+    this.configureDatabase();
+    this.configureMiddleware();
+    this.configureRoutes();
+
+    return this.app;
+  }
   private static app: Application = express();
 
   private static configureServices(): void {
@@ -30,38 +36,29 @@ export default class Server {
 
   private static configureDatabase(): void {
     createConnection().catch(error =>
-      console.log("Error when trying to create a database", error)
+      signale.error("Error when trying to create a database", error)
     );
   }
 
   private static configureRoutes(): void {
     useExpressServer(this.app, {
-      cors: true,
-      controllers: [`${__dirname}/**/*Controller.{ts,js}`]
+      controllers: [`${__dirname}/**/*Controller.{ts,js}`],
+      cors: true
     });
   }
 
   private static configureMiddleware(): void {
     this.app.use(cors());
 
-    // this.app.use(compression());
-    // this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(compression());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(bodyParser.json());
-    // this.app.use(helmet());
+    this.app.use(helmet());
     this.app.use(morgan("dev"));
     this.app.use(
       "/api-docs",
       swaggerUi.serve,
       swaggerUi.setup(swaggerDocument)
     );
-  }
-
-  static bootstrap(): Application {
-    this.configureServices();
-    this.configureDatabase();
-    this.configureMiddleware();
-    this.configureRoutes();
-
-    return this.app;
   }
 }
